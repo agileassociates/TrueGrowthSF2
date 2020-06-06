41ee880a28ea2bb48ce7d96f9540d856c9d0c5a7
Johny Babylon <Home@Johnys-MacBook.local>
2020-06-06 16:22:32 +0700

    fixed tab bar icons

--- TrueGrowthSF2/Controller/CameraViewController.swift
+++ TrueGrowthSF2/Controller/CameraViewController.swift
@@ -120,29 +120,10 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
                 } else {
                     self.dismiss(animated: true, completion: nil)
                 }
-
             }
         }
-        
     }
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    
-    
-    
-    
     
     //didnt use....maybe use when refactor
     func sendDataToDatabase(photoUrl: String) {
